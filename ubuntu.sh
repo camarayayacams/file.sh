592818676c317dd32d5989846434e849865101fa
camarayayacams <136078747+camarayayacams@users.noreply.github.com>
2023-06-26 03:35:27 +0000

    ubuntu.sh

---
+++ ubuntu.sh
@@ -0,0 +1,12 @@
+#!usr/bin/bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
+sudo service docker start
+sudo docker run hello-world
+docker pull ubuntu
